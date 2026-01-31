12fd7d729b9e449684daf560170f7da82830ae92
Timofey Zakharchuk <timgooffical@gmail.com>
2026-01-31 23:53:40 +0300

    Update floatdef.h

--- include/floatdef.h
+++ include/floatdef.h
@@ -1,8 +1,13 @@
 #pragma once
+#include<immintrin.h>
 #if defined(NEXT_FP64)
     using real = double;
 #elif defined(NEXT_FP32)
     using real = float;
+#elif defined(NEXT_SIMD64)
+    using real = __m128d;
+#elif defined(NEXT_SIMD32) 
+    using real = __m128;
 #else
-    #error "You must define NEXT_FP64 or NEXT_FP32 to proceed."
+    #error "You must define NEXT_FP64, NEXT_FP32, NEXT_SIMD32 or NEXT_SIMD64 to proceed."
 #endif
