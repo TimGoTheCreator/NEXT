591d9470a7498b050a4c274179fe48bda8417d30
Timofey Zakharchuk <timgooffical@gmail.com>
2026-01-31 23:36:21 +0300

    Add files via upload

---
+++ src/gravity/step.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../struct/particle.h"
+#include <vector>
+
+inline void Step(std::vector<Particle>& p, real dt) {
+    const int count = p.size();
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t j = i + 1; j < count;)
+        {
+            Gravity(p[i], p[j], dt);
+        }
+        
+    }
+
+    for (size_t i = 0; i < count; i++)
+    {
+        p[i].x += p[i].vx * dt;
+        p[i].y += p[i].vy * dt;
+        p[i].z += p[i].vz * dt;
+    }
+    
+    
+}
