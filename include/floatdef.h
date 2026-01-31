6263ad36a99f1299e0f50558eba5d7fbd8fe1cc8
Timofey Zakharchuk <timgooffical@gmail.com>
2026-01-31 23:59:17 +0300

    Update floatdef.h

--- include/floatdef.h
+++ include/floatdef.h
@@ -1,16 +1,22 @@
 #pragma once
-#include<immintrin.h>
+#include <immintrin.h>
+
 #if defined(NEXT_FP64)
     using real = double;
+
 #elif defined(NEXT_FP32)
     using real = float;
+
 #elif defined(NEXT_SIMD64)
     using real = __m128d;
-#elif defined(NEXT_SIMD32) 
+
+#elif defined(NEXT_SIMD32)
     using real = __m128;
+
 #else
     #error "You must define NEXT_FP64, NEXT_FP32, NEXT_SIMD32 or NEXT_SIMD64 to proceed."
 #endif
+
 
 #if defined(NEXT_SIMD32)
 
@@ -28,7 +34,6 @@ inline real div(real a, real b) { return _mm_div_pd(a, b); }
 
 #else
 
-// scalar fallback
 inline real add(real a, real b) { return a + b; }
 inline real sub(real a, real b) { return a - b; }
 inline real mul(real a, real b) { return a * b; }
