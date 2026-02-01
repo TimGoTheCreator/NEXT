8fe4872a0208882abc9c32f24bcf4de761138885
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-01 18:38:34 +0300

    Update coldcollapse.py

--- examples/ColdCollapseGalaxy/coldcollapse.py
+++ examples/ColdCollapseGalaxy/coldcollapse.py
@@ -1,10 +1,10 @@
 import os, sys
+
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 if root not in sys.path:
-
     sys.path.insert(0, root)
 
-from tools.icbuilder import particleAdd, save, coldPlummer
+from tools.icbuilder import coldPlummer, save
 
-coldPlummer(1000, scale=1.0, mass=1.0)
-save(coldcollapse.txt) 
+particles = coldPlummer(1000, scale=1.0, mass=1.0)
+save("coldcollapse.txt", particles)
