962d04b4bb20f3ee0cb5660260d3a0ea338b9ad8
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-01 16:13:32 +0300

    Update galaxy.py

--- examples/GalaxyDemo/galaxy.py
+++ examples/GalaxyDemo/galaxy.py
@@ -4,7 +4,7 @@ if root not in sys.path:
 
     sys.path.insert(0, root)
 
-from tools.icbuilder import particleAdd, save, plummer
+from tools.icbuilder import particleAdd, save, disk
 
-plummer(1000, 1)
+disk(20000, radius=0.5, mass=1.0, thickness=0.2) 
 save("galaxy.txt") 
