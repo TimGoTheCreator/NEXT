1308022eb4668b099793b475dc0be2d9d696ba6b
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-01 11:22:05 +0300

    Changed variable naming to unsigned

--- src/gravity/step.h
+++ src/gravity/step.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 inline void Step(std::vector<Particle>& p, real dt) {
-    const int count = p.size();
+    const size_t count = p.size();
     for (size_t i = 0; i < count; i++)
     {
         for (size_t j = i + 1; j < count; j++)
