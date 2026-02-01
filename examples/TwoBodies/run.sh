c55ead7405b2925e82082dbe92472b03255638d3
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-01 21:08:08 +0300

    Update run.sh

--- examples/TwoBodies/run.sh
+++ examples/TwoBodies/run.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# Absolute path to the directory containing this script
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# Project root = two levels up
-ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
-
-# Run the executable from the root
-"$ROOT/next" "$SCRIPT_DIR/two_bodies.txt"
+../../next two_body.txt 0.001 1
