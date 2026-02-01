f6c441ea4f80e873e3f74983064ca34b495b026a
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-01 18:39:09 +0300

    Update galaxy.py

--- examples/GalaxyDemo/galaxy.py
+++ examples/GalaxyDemo/galaxy.py
@@ -1,10 +1,10 @@
 import os, sys
+
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 if root not in sys.path:
-
     sys.path.insert(0, root)
 
-from tools.icbuilder import particleAdd, save, disk
+from tools.icbuilder import disk, save
 
-disk(20000, radius=0.5, mass=1.0, thickness=0.2) 
-save("galaxy.txt") 
+particles = disk(20000, radius=0.5, mass=1.0, thickness=0.2)
+save("galaxy.txt", particles)
