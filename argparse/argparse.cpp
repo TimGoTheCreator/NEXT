0861660292b847843b8414be95be37275af12cf0
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-02 22:51:50 +0300

    Update argparse.cpp

--- argparse/argparse.cpp
+++ argparse/argparse.cpp
@@ -7,12 +7,20 @@ namespace next {
 Arguments parse_arguments(int argc, char** argv)
 {
     if (argc != 6) {
-        std::cerr << "Usage: next <initial.txt> <dt> <dump_interval> <vtk|vtu>\n";
+        std::cerr << "Usage: next <input.txt> <threads> <dt> <dump_interval> <vtk|vtu>\n";
         std::exit(1);
     }
 
     Arguments args;
-    args.input_file = argv[2];
+
+    args.input_file = argv[1];
+
+    args.threads = std::stoi(argv[2]);
+    if (args.threads < 1) {
+        std::cerr << "Thread count must be >= 1\n";
+        std::exit(1);
+    }
+
     args.dt = std::stod(argv[3]);
     args.dump_interval = std::stod(argv[4]);
 
