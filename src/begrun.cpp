e52e5041c9070803465d63aa36d7e91a31f1ff3e
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-02 19:16:41 +0300

    Refactor main function and load particles with argparse

--- src/begrun.cpp
+++ src/begrun.cpp
@@ -1,69 +1,41 @@
 #include "gravity/step.h"
-#include <iostream>
-#include <fstream>
 #include "floatdef.h"
 #include "dt/adaptive.h"
 #include "io/vtk_save.h"
 #include "io/vtu_save.h"
-
-std::vector<Particle> LoadParticlesFromFile(const std::string& filename)
-{
-    std::vector<Particle> p;
-    std::ifstream in(filename);
-
-    Particle temp;
-    while (in >> temp.x >> temp.y >> temp.z >> temp.vx >> temp.vy >> temp.vz >> temp.m)
-        p.push_back(temp);
-
-    return p;
-}
+#include "io/load_particle.hpp"
+#include "argparse/argparse.hpp"
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char** argv)
 {
-    if (argc != 5)
-    {
-        std::cerr << "Usage: next <initial.txt> <dt> <dump_interval> <vtk|vtu>\n";
-        return 1;
-    }
+    auto args = next::parse_arguments(argc, argv);
 
-    const char* filename = argv[1];
-    real dt = std::stod(argv[2]);
-    double cacheInterval = std::stod(argv[3]);
-    std::string format = argv[4];
-
-    bool useVTU = (format == "vtu");
+    std::vector<Particle> particles = LoadParticlesFromFile(args.input_file);
 
     real simTime = 0;
     real nextDump = 0;
-
-    std::vector<Particle> particles = LoadParticlesFromFile(filename);
-
     int step = 0;
 
     while (true)
     {
-        real dtAdaptive = computeAdaptiveDt(particles, dt);
+        real dtAdaptive = computeAdaptiveDt(particles, args.dt);
         Step(particles, dtAdaptive);
         simTime += dtAdaptive;
 
         if (simTime >= nextDump)
         {
-            std::string out;
+            std::string out = "dump_" + std::to_string(step) + (args.use_vtu ? ".vtu" : ".vtk");
 
-            if (useVTU)
-            {
-                out = "dump_" + std::to_string(step) + ".vtu";
+            if (args.use_vtu)
                 SaveVTU(particles, out);
-            }
             else
-            {
-                out = "dump_" + std::to_string(step) + ".vtk";
                 SaveVTK(particles, out);
-            }
 
             std::cout << "Wrote: " << out << "\n";
 
-            nextDump += cacheInterval;
+            nextDump += args.dump_interval;
             step++;
         }
     }
