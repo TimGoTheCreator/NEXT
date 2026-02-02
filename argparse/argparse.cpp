089a96b81f58cb3291e3c9b240a05e0b2f894bcd
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-02 22:51:00 +0300

    Update argparse.cpp

--- argparse/argparse.cpp
+++ argparse/argparse.cpp
@@ -6,17 +6,17 @@ namespace next {
 
 Arguments parse_arguments(int argc, char** argv)
 {
-    if (argc != 5) {
+    if (argc != 6) {
         std::cerr << "Usage: next <initial.txt> <dt> <dump_interval> <vtk|vtu>\n";
         std::exit(1);
     }
 
     Arguments args;
-    args.input_file = argv[1];
-    args.dt = std::stod(argv[2]);
-    args.dump_interval = std::stod(argv[3]);
+    args.input_file = argv[2];
+    args.dt = std::stod(argv[3]);
+    args.dump_interval = std::stod(argv[4]);
 
-    std::string fmt = argv[4];
+    std::string fmt = argv[5];
     if (fmt == "vtu") args.use_vtu = true;
     else if (fmt == "vtk") args.use_vtu = false;
     else {
