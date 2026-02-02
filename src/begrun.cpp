8db9a9c0a206834046368d38535c2c0fd45e4f5a
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-02 18:13:49 +0300

    Added VTU support

--- src/begrun.cpp
+++ src/begrun.cpp
@@ -1,9 +1,10 @@
 #include "gravity/step.h"
-#include<iostream>
-#include<fstream>
-#include"floatdef.h"
-#include"dt/adaptive.h"
-#include"io/vtk_save.h"
+#include <iostream>
+#include <fstream>
+#include "floatdef.h"
+#include "dt/adaptive.h"
+#include "io/vtk_save.h"
+#include "io/vtu_save.h"
 
 std::vector<Particle> LoadParticlesFromFile(const std::string& filename)
 {
@@ -11,34 +12,27 @@ std::vector<Particle> LoadParticlesFromFile(const std::string& filename)
     std::ifstream in(filename);
 
     Particle temp;
-    while(in >> temp.x >> temp.y >> temp.z >> temp.vx >> temp.vy >> temp.vz >> temp.m)
-    {
+    while (in >> temp.x >> temp.y >> temp.z >> temp.vx >> temp.vy >> temp.vz >> temp.m)
         p.push_back(temp);
-    }
+
     return p;
 }
 
-int main(int argc, char** argv) {
-
-    std::cout
-    << "NN   NN  EEEEEEE  XX   XX  TTTTTTT\n"
-    << "NNN  NN  EE        XX XX     TTT  \n"
-    << "NN N NN  EEEEE      XXX      TTT  \n"
-    << "NN  NNN  EE        XX XX     TTT  \n"
-    << "NN   NN  EEEEEEE  XX   XX    TTT  \n"
-    << "Newtonian EXact Trajectories\n";
-    // Print the Splash
-
-    if (argc != 4) // if there wasnt 3 of parameters set, throw a cerr
+int main(int argc, char** argv)
+{
+    if (argc != 5)
     {
-        std::cerr << "No parameter file specified\n";
-        std::cerr << "Usage: next <initial.txt> <dt> <dump_interval>\n";
+        std::cerr << "Usage: next <initial.txt> <dt> <dump_interval> <vtk|vtu>\n";
         return 1;
     }
 
     const char* filename = argv[1];
     real dt = std::stod(argv[2]);
     double cacheInterval = std::stod(argv[3]);
+    std::string format = argv[4];
+
+    bool useVTU = (format == "vtu");
+
     real simTime = 0;
     real nextDump = 0;
 
@@ -54,16 +48,25 @@ int main(int argc, char** argv) {
 
         if (simTime >= nextDump)
         {
-            std::string out = "dump_" + std::to_string(step) + ".vtk";
-            SaveVTK(particles, out); // save the file
-            std::cout << "Wrote: " << out << "\n"; // say you wrote the file
-            nextDump += cacheInterval;
-        }
+            std::string out;
 
-        step++; // increase counter by 1
+            if (useVTU)
+            {
+                out = "dump_" + std::to_string(step) + ".vtu";
+                SaveVTU(particles, out);
+            }
+            else
+            {
+                out = "dump_" + std::to_string(step) + ".vtk";
+                SaveVTK(particles, out);
+            }
+
+            std::cout << "Wrote: " << out << "\n";
+
+            nextDump += cacheInterval;
+            step++;
+        }
     }
 
-    return 0; // end if succeeded, well probably not it never ends anyway. 
-    
-    
+    return 0;
 }
