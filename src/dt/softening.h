c072520c217ba794c99c484a374c7fd2422c6544
Timofey Zakharchuk <timgooffical@gmail.com>
2026-02-03 14:51:03 +0200

    Added pairwise softening
    Add pairwise softening for better simulation stability to use in src/struct/particle.h

--- src/dt/softening.h
+++ src/dt/softening.h
@@ -2,12 +2,10 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -40,4 +38,21 @@ inline real nextSoftening(real nodeSize,
         eps = eps_min;
 
     return eps;
+}
+
+inline real pairSoftening(real ma, real mb)
+{
+    // Mass-based softening (cubic root gives physical size scale)
+    real ea = std::cbrt(ma) * real(0.002);
+    real eb = std::cbrt(mb) * real(0.002);
+
+    // Symmetric combination
+    real eps2 = ea*ea + eb*eb;
+
+    // Minimum floor
+    const real eps_min = real(1e-6);
+    if (eps2 < eps_min)
+        eps2 = eps_min;
+
+    return eps2;
 }
