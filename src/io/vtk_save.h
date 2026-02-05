7df6f0ee24190bd6b13a2b88e1cd5ac556f15911
Timofey Zakharchuk <timofey.zakharchuk.research@gmail.com>
2026-02-05 21:18:18 +0200

    add "double" writer

--- src/io/vtk_save.h
+++ src/io/vtk_save.h
@@ -2,12 +2,10 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -28,8 +26,14 @@ inline void SaveVTK(const std::vector<Particle>& p, const std::string& filename)
     out << "ASCII\n";
     out << "DATASET POLYDATA\n";
 
+    #ifdef NEXT_FP64
+    constexpr const char* vtkType = "double";
+    #elif defined(NEXT_FP32)
+    constexpr const char* vtkType = "float";
+    #endif
+
     // draw points
-    out << "POINTS " << N << " float\n";
+    out << "POINTS " << N << " " << vtkType << "\n";
     for (const auto& a : p)
         out << a.x << " " << a.y << " " << a.z << "\n";
 
@@ -40,12 +44,14 @@ inline void SaveVTK(const std::vector<Particle>& p, const std::string& filename)
 
     // that too
     out << "POINT_DATA " << N << "\n";
-    out << "VECTORS velocity float\n";
+    out << "VECTORS velocity " << vtkType << "\n";
     for (const auto& a : p)
         out << a.vx << " " << a.vy << " " << a.vz << "\n";
 
-    out << "SCALARS mass float 1\n";
+    out << "SCALARS mass " << vtkType << " 1\n";
     out << "LOOKUP_TABLE default\n";
     for (const auto& a : p)
         out << a.m << "\n";
+
+    out.close();
 }
