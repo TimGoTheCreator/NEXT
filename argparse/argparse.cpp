6dffb458eff45f83366ad1cdd8d21da02cb3e93f
Timofey Zakharchuk <timofey.zakharchuk.research@gmail.com>
2026-02-14 18:13:11 +0300

    Update argparse.cpp

--- argparse/argparse.cpp
+++ argparse/argparse.cpp
@@ -7,32 +7,31 @@ namespace next {
 Arguments parse_arguments(int argc, char** argv)
 {
     if (argc != 6) {
-        std::cerr << "Usage: next <input.txt> <threads> <dt> <dump_interval> <vtk|vtu>\n";
+        std::cerr << "Usage: next <input.txt> <threads> <dt> <dump_interval> <vtk|vtu|hdf5>\n";
         std::exit(1);
     }
 
     Arguments args;
 
     args.input_file = argv[1];
-
     args.threads = std::stoi(argv[2]);
-    if (args.threads < 1) {
-        std::cerr << "Thread count must be >= 1\n";
-        std::exit(1);
-    }
-
     args.dt = std::stod(argv[3]);
     args.dump_interval = std::stod(argv[4]);
 
     std::string fmt = argv[5];
-    if (fmt == "vtu") args.use_vtu = true;
-    else if (fmt == "vtk") args.use_vtu = false;
+
+    if (fmt == "vtk")
+        args.format = OutputFormat::VTK;
+    else if (fmt == "vtu")
+        args.format = OutputFormat::VTU;
+    else if (fmt == "hdf5")
+        args.format = OutputFormat::HDF5;
     else {
-        std::cerr << "Format must be 'vtk' or 'vtu'\n";
+        std::cerr << "Choose a file format: vtk, vtu, or hdf5\n";
         std::exit(1);
     }
 
     return args;
 }
 
-}
+} // namespace next
