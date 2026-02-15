83f37efb9c884953a25474bef48be8391ff82b72
Timofey Zakharchuk <timofey.zakharchuk.research@gmail.com>
2026-02-15 14:03:05 +0200

    Update octree.h

--- src/gravity/octree.h
+++ src/gravity/octree.h
@@ -10,6 +10,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
+#include "struct/particle.h"
 #include "floatdef.h"
 #include "dt/softening.h"
 #include <vector>
